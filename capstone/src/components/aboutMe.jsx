ce8626f113fc46cdcbe38dbdf2ad1924be1afe00
lucymutunga <lucyalphonce18@gmail.com>
2024-03-26 21:44:28 +0300

    adding autobiography

--- capstone/src/components/aboutMe.jsx
+++ capstone/src/components/aboutMe.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 
 const AboutMe = () => {
+  const pdfUrl = "./auto.pdf";
+
   return (
     <div className=" flex  justify-around  items-center  bg-white font-bold space-x-2 px-2  mt-6 md:w-svw md:space-x-36 lg:space-x-70  lg:text-2xl">
-      <a href="">
-        <button className="rounded-full bg-red-400  w-20 h-20 md:w-28 md:h-28 md:mr-10 lg:w-40 lg:h-40">
-          Autobio <br /> graphy
+      <a
+        // href="./assets/Lucy%20Mutunga's%20Autobiography.pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button
+          className="rounded-full bg-red-400 w-20 h-20 md:w-28 md:h-28 md:mr-10 lg:w-40 lg:h-40"
+          onClick={() => (window.location.href = "./auto.pdf")}
+        >
+          Autobio <br />
+          graphy
         </button>
       </a>
+      {/* <a href={pdfUrl} target="_blank">
+        Download PDF
+      </a> */}
+
       <a href="">
         <button className="rounded-full bg-red-400  w-20 h-20 px-2 md:w-28 md:h-28 lg:w-40 lg:h-40">
           Power
